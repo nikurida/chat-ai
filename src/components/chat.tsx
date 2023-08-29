3b3686e1d657ed7bd7b6746e297b2a447c4e0210
Ricardo <rikurida@gmail.com>
2023-08-28 22:37:12 -0400

    finished open ai integration and configuration

---
+++ src/components/chat.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+
+import { useChat } from "ai/react";
+
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    api: "/api/chat",
+  });
+
+  return (
+    <div className="flex min-h-screen bg-slate-50 items-center justify-center">
+      <Card className="w-[440px] h-[700px] grid grid-rows-[min-content_1fr_min-content]">
+        <CardHeader>
+          <CardTitle>Chat AI</CardTitle>
+          <CardDescription>
+            Using Vercel SDK to create a chat bot.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          {messages.map((messages) => {
+            return (
+              <div
+                className="flex gap-3 text-slate-600 text-sm"
+                key={messages.id}
+              >
+                {messages.role == "user" && (
+                  <Avatar>
+                    <AvatarFallback>RA</AvatarFallback>
+                    <AvatarImage src="https://github.com/nikurida.png" />
+                  </Avatar>
+                )}
+
+                {messages.role == "assistant" && (
+                  <Avatar>
+                    <AvatarFallback>IA</AvatarFallback>
+                    <AvatarImage src="https://pbs.twimg.com/media/Cbx-oKvW8AACd-D.jpg" />
+                  </Avatar>
+                )}
+
+                <p className="leading-relaxed">
+                  <span className="block font-bold text-slate-800 ">
+                    {messages.role == "user" ? "User" : "AI"}
+                  </span>
+                  {messages.content}
+                </p>
+              </div>
+            );
+          })}
+        </CardContent>
+        <CardFooter>
+          <form className="flex gap-2 w-full" onSubmit={handleSubmit}>
+            <Input
+              placeholder="How can I help you?"
+              value={input}
+              onChange={handleInputChange}
+            />
+            <Button type="submit">Send</Button>
+          </form>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
