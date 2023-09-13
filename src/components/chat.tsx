a26bae40401972717dea55e0d13a5d850dfaa3dc
Ricardo <rikurida@gmail.com>
2023-09-12 20:54:20 -0400

    fix scroll area

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -13,6 +13,7 @@ import {
 import { Input } from "@/components/ui/input";
 
 import { useChat } from "ai/react";
+import { ScrollArea } from "./ui/scroll-area";
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat({
@@ -21,43 +22,45 @@ export default function Chat() {
 
   return (
     <div className="flex min-h-screen bg-slate-50 items-center justify-center">
-      <Card className="w-[440px] h-[700px] grid grid-rows-[min-content_1fr_min-content]">
+      <Card className="w-[440px]">
         <CardHeader>
           <CardTitle>Chat AI</CardTitle>
           <CardDescription>
             Using Vercel SDK to create a chat bot.
           </CardDescription>
         </CardHeader>
-        <CardContent className="space-y-4">
-          {messages.map((messages) => {
-            return (
-              <div
-                className="flex gap-3 text-slate-600 text-sm"
-                key={messages.id}
-              >
-                {messages.role == "user" && (
-                  <Avatar>
-                    <AvatarFallback>RA</AvatarFallback>
-                    <AvatarImage src="https://github.com/nikurida.png" />
-                  </Avatar>
-                )}
+        <CardContent>
+          <ScrollArea className="h-[600px] w-full pr-4">
+            {messages.map((messages) => {
+              return (
+                <div
+                  className="flex gap-3 text-slate-600 text-sm mb-4"
+                  key={messages.id}
+                >
+                  {messages.role == "user" && (
+                    <Avatar>
+                      <AvatarFallback>RA</AvatarFallback>
+                      <AvatarImage src="https://github.com/nikurida.png" />
+                    </Avatar>
+                  )}
 
-                {messages.role == "assistant" && (
-                  <Avatar>
-                    <AvatarFallback>IA</AvatarFallback>
-                    <AvatarImage src="https://pbs.twimg.com/media/Cbx-oKvW8AACd-D.jpg" />
-                  </Avatar>
-                )}
+                  {messages.role == "assistant" && (
+                    <Avatar>
+                      <AvatarFallback>IA</AvatarFallback>
+                      <AvatarImage src="https://pbs.twimg.com/media/Cbx-oKvW8AACd-D.jpg" />
+                    </Avatar>
+                  )}
 
-                <p className="leading-relaxed">
-                  <span className="block font-bold text-slate-800 ">
-                    {messages.role == "user" ? "User" : "AI"}
-                  </span>
-                  {messages.content}
-                </p>
-              </div>
-            );
-          })}
+                  <p className="leading-relaxed">
+                    <span className="block font-bold text-slate-800 ">
+                      {messages.role == "user" ? "User" : "AI"}
+                    </span>
+                    {messages.content}
+                  </p>
+                </div>
+              );
+            })}
+          </ScrollArea>
         </CardContent>
         <CardFooter>
           <form className="flex gap-2 w-full" onSubmit={handleSubmit}>
